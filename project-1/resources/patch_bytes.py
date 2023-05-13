d4f4eb8da8fb8effdf864db3d971630fb71aa0a6
Radu Dilirici <diliriciradu@gmail.com>
2023-05-13 22:32:16 +0300

    Remove source files

--- project-1/resources/patch_bytes.py
+++
@@ -1,9 +0,0 @@
-import idaapi
-
-with open('./function.out', 'rb') as f:
-    addr = 0x401842
-    byte = f.read(1)
-    while byte:
-        idaapi.patch_byte(addr, ord(byte))
-        byte = f.read(1)
-        addr += 1
